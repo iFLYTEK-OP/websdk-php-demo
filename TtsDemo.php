78ff2298911f38053a33e3c7b29abb54bd595531
wowo_zZ <zhenggui5228@126.com>
2021-04-12 18:58:15 +0800

    update

--- TtsDemo.php
+++ TtsDemo.php
@@ -2,5 +2,9 @@
 
 include './vendor/autoload.php';
 
-$client = new IFlytek\Xfyun\Speech\TtsClient('', $api_key, $api_secret, ['vcn' => 'x2_qige']);
+$appId = '';// 需要填入app_id
+$apiKey = '';// 需要填入api_key
+$apiSecret = '';// 需要填入api_secret
+
+$client = new IFlytek\Xfyun\Speech\TtsClient($appId, $apiKey, $apiSecret);
 file_put_contents('./result.mp3', $client->request('欢迎使用科大讯飞语音能力，让我们一起用人工智能改变世界')->getBody());
